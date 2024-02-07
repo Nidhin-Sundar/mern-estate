e6a9a38e38dd9561d7e9a708f653b766a9d96a3e
Nidhin.S <nidhin.sundar@seqato.com>
2024-02-07 10:34:47 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
 
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
-            type="text"
+             type="text"
             placeholder="Search...."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
