1378ad36c6e49e8d3f63556a1884d93e4624fbea
Nidhin.S <nidhin.sundar@seqato.com>
2024-02-15 16:07:08 +0530

    Update the header and make the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  console.log(currentUser, "adfa");
   return (
     <header className="bg-green-700 shadow-md ">
       <div className="flex  justify-between items-center max-w-xl mx-auto p-3">
@@ -15,7 +18,7 @@ export default function Header() {
 
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
-             type="text"
+            type="text"
             placeholder="Search...."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
@@ -23,21 +26,23 @@ export default function Header() {
         </form>
         <ul className=" flex gap-3">
           <Link to="/">
-          <li className="hidden sm:inline text-slate-900 hover:underline">
-            Home
-          </li>
+            <li className="hidden sm:inline text-slate-900 hover:underline">
+              Home
+            </li>
           </Link>
 
           <Link to="/about">
-          
-          <li className="hidden sm:inline text-slate-900 hover:underline">
-            About
-          </li>
+            <li className="hidden sm:inline text-slate-900 hover:underline">
+              About
+            </li>
           </Link>
 
-          <Link to="sign-in">
-          
-          <li className=" text-slate-900 hover:underline">Sign in</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar} alt="profile" />
+            ) : (
+              <li className=" text-slate-900 hover:underline">Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
